1cc1c3052f455e8d1c261ff03997bd5c279dc488
themathboi101 <78664310+themathboi101@users.noreply.github.com>
2021-05-20 15:48:58 -0400

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -19,29 +19,19 @@ class Slingshot{
     }
 
     display(){
-        //image(this.sling1,200,20);
-        //image(this.sling2,170,20);
+        
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            push();
-            
-            stroke(48,22,8);
-            
-                strokeWeight(7);
-                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
-               line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
-                
-            }
-           else{
-                strokeWeight(3);
-                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
-                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
+            strokeWeight(3);
+            line(pointA.x - 10, pointA.y, pointB.x -10, pointB.y);
                
-            }
-           
-            
-            pop();
+                
         }
     }
-   
+        fly(){
+        this.sling.bodyA=null;
+        
+    }
+}
